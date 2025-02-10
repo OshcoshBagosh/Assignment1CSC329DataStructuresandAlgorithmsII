0fb38a38dc449cec17be64b7ff4c603d1a88667d
OshcoshBagosh <oscargabrguerrero@gmail.com>
2025-02-09 20:40:06 -0500

    PQList

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,10 +5,21 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         Player player1 = new Player("John", 27);
-        Player player2 = new Player(player1);
+        Player player2 = new Player("Steve", 39);
+        Player player3 = new Player("Joe", 20);
+        Player player4 = new Player("Mark", 87);
+        Player player5 = new Player("Simon", 1);
+        Player player6 = new Player("Logan", 100);
 
-        System.out.println(player1.getName());
-        System.out.println(player2.getName());
-        System.out.println(player1.equals(player2));
+        PQList list = new PQList();
+        list.add(player1);
+        list.add(player2);
+        list.add(player3);
+        list.add(player4);
+        list.add(player5);
+        list.add(player6);
+
+        System.out.println(list.getSize());
+        System.out.println(list.getHighestScorePlayer().getName());
     }
 }
--- src/main/java/org/example/PQList.java
+++ src/main/java/org/example/PQList.java
@@ -11,23 +11,34 @@ public class PQList implements PriorityQueue {
 
     private Node head;
     private Node tail;
+    private Node priority;
     private int length;
+    private int highScore;
 
     public PQList(){
         this.head = null;
         this.tail = null;
         this.length = 0;
+        this.priority = null;
+        this.highScore = 0;
     }
 
     public PQList(Node node){
         this.head = node;
         this.tail = node;
         this.length = 1;
+        this.highScore = 0;
+        this.priority = node;
+        this.highScore = node.data.getScore();
     }
 
     @Override
     public void add(Player a) {
         Node node = new Node(a);
+        if(this.highScore < node.data.getScore()){
+            this.highScore = node.data.getScore();
+            this.priority = node;
+        }
         if (this.head == null){
           this.head = node;
         }
@@ -55,5 +66,43 @@ public class PQList implements PriorityQueue {
         if(this.length == 0)
             return true;
         return false;
+    }
+    private void searchHighScore(){
+        this.highScore = 0;
+        Node temp = this.head;
+        for(int i = 0; i < this.length; i++){
+            if (this.highScore < temp.data.getScore()){
+                this.highScore = temp.data.getScore();
+                priority = temp;
+            }
+            temp = temp.next;
+        }
+    }
+
+    public Player getPriority(){
+        return this.priority.data;
+    }
+
+    @Override
+    public Player getHighestScorePlayer() {
+        Node temp = this.head;
+        while (temp != null){
+            if (temp == this.priority){
+                temp = this.priority;
+                this.head = head.next;
+                this.length -= 1;
+                searchHighScore();
+                return temp.data;
+            }
+            else if (temp.next == this.priority){
+                temp.next = temp.next.next;
+                temp = this.priority;
+                this.length -= 1;
+                searchHighScore();
+                return temp.data;
+            }
+            temp = temp.next;
+        }
+        return null;
     }
 }
